afc8135af76fd1a00fb5bb24c1f968c9cdd9a7aa
justgit <justgit@gmail.com>
2021-10-08 00:09:47 +0530

    'criticalTable'

--- doctor-patient-system/src/components/CriticalTable.js
+++ doctor-patient-system/src/components/CriticalTable.js
@@ -1,8 +1,37 @@
-import React from "react";
-import Table from "react-bootstrap/Table";
+import React, { useEffect, useState } from "react";
+import { Button, Modal, Table } from "react-bootstrap";
 import "./styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const axios = require('axios');
+const qs = require('qs');
 
 export default function CriticalTable() {
+  
+
+    const [data, setData] = useState([]);
+  
+    const fetchAppointments = async () => {
+       
+  
+        try {
+            const resp = await axios.post('/fetchCritical');
+            
+            let dataObj = JSON.parse(resp.data.data);
+            console.log(dataObj);
+            setData(dataObj);
+        } catch (err) {
+            // Handle Error Here
+            console.error(err);
+        }
+    };
+    
+    useEffect(()=>{
+        fetchAppointments();
+    }, [])
+  
+
+  
   return (
     <>
       <br />
@@ -21,33 +50,19 @@ export default function CriticalTable() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>1</td>
-              <td>Rakesh</td>
-              <td>19:30</td>
-              <td>Multiple Trauma</td>
-              <td>Medicine</td>
-              <td>Critical</td>
-              <td>1700</td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>Rakesh</td>
-              <td>19:30</td>
-              <td>Multiple Trauma</td>
-              <td>Medicine</td>
-              <td>Critical</td>
-              <td>1700</td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>Rakesh</td>
-              <td>19:30</td>
-              <td>Multiple Trauma</td>
-              <td>Medicine</td>
-              <td>Critical</td>
-              <td>1700</td>
-            </tr>
+          {data.map((item) => (
+          <tr key={item.id}>
+            <td>{item.patient_id}</td>
+            <td>{item.pname}</td>
+            <td>{item.time}</td>
+            <td>{item.diagnosis}</td>
+            <td>{item.prescription}</td>
+            <td>{item.condition}</td>
+            <td>{item.amount}</td>
+            
+            
+          </tr>
+        ))}
           </tbody>
         </Table>
       </div>
